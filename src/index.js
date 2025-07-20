98e02236f760d484535f757a98955b4105cdeca5
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-07-21 00:10:05 +0600

    fix: some variable issue fixed

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 const previousButton = document.querySelector("#previous")
 const nextButton = document.querySelector("#next")
-const imageHolder = document.querySelector("#image")
+const imageHolder = document.querySelector("#image") 
 
-const image1 = "./assets/playingRabbit.png"
-const image2 = "./assets/pondDog.png"
-const image3 = "./assets/Rabbit.png"
-const image4 = "./assets/sleepingCat.png"
-const image5 = "./assets/yellowCat.png"
+const images = [
+    "./assets/playingRabbit.png",
+    "./assets/pondDog.png",
+    "./assets/Rabbit.png", 
+    "./assets/sleepingCat.png", 
+    "./assets/yellowCat.png"
+]
 
-const images = [image1, image2, image3, image4, image5]
-
-let previousImage = 0
 let currentImage = 0
-let nextImage = 0
 
 previousButton.addEventListener("click", ()=>{
-    if(previousImage < 1) {
-        previousImage = images.length - 1
+    if(currentImage < 1) {
+        currentImage = images.length - 1
     }else {
-        previousImage -= 1
+        currentImage--
     }
-    currentImage = previousImage
     updateImage()
 })
 nextButton.addEventListener("click", ()=>{
-    if(nextImage > images.length - 2) {
-        nextImage = 0
+    if(currentImage > images.length - 2) {
+        currentImage = 0
     }else {
-        nextImage += 1
+        currentImage++
     }
-    currentImage = nextImage
     updateImage()
 })
 
 function updateImage() {
     imageHolder.setAttribute("src", images[currentImage])
 }
+updateImage()
